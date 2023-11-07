19fd9be945c98851ab356f67db399ad5394d76b7
RangCloud <mtr008@naver.com>
2023-11-07 09:25:21 +0900

    231107

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,13 @@ import styles from '@/styles/Home.module.css'
 export default function Home() {
   return (
     <div>
-      hello next.js
+      <h1>/page/index.js</h1>
+      <ul>
+        <li><a href='/sub'>/page/index.js</a></li>
+        <li><a href='/sub/about'>/page/sub/about.js</a></li>
+        <li><a href='/sub/1'>/page/sub/[id].js</a></li>
+        <li><a href='/sub/2'>/page/sub/[id].js</a></li>
+      </ul>
     </div>
   )
 }
---
+++ src/pages/sub/about.js
@@ -0,0 +1,10 @@
+function SubAbout(){
+    return(
+        <div>
+            <h1>/page/sub/about.js</h1>
+            <a href='/'>page/index.js</a>
+        </div>
+    )
+}
+
+export default SubAbout;
